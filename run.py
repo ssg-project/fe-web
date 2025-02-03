05b97a27d1d1df596f5d037e38eadedeef1bad01
simsangho6448 <mjj00travel@gmail.com>
2025-02-03 16:36:30 +0900

    socket.io 삭제

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app.app import create_app
-from flask_socketio import SocketIO
 
 app = create_app()
-socketio = SocketIO(app)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
