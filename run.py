719346c802b5678f6bf85da9e2282d5e57613116
myeongsoyi <myeongsoyi@gmail.com>
2025-02-04 15:06:48 +0900

    feat: healthcheck 엔드포인트 등록

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app.app import create_app
 
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}, 200
+
 app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-@app.get("/health")
-async def health_check():
-    return {"status": "ok"}, 200
